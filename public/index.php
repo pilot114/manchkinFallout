cbce2d8ec94e30f7df3f752407498f1e6baeda16
pilot114 <pilot114@bk.ru>
2014-08-07 00:07:06 +0700

    up

--- public/index.php
+++ public/index.php
@@ -3,14 +3,9 @@
 include '../vendor/autoload.php';
 $cards = include '../src/mapping.php';
 
-$number = 20;
+$number = rand(0,279);
 
 var_dump($cards[$number]);
 
-echo '<img src="card/result/' . $number . '.png" width="250">';
-
-foreach (glob("card/result/0*.png") as $filename) {
-    $filename = basename($filename);
-    $files[] = $filename;
-}
-echo count($files);
+echo $number;
+echo '<br><img src="card/mini/' . $number . '.png" width="250">';
