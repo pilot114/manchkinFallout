e98aaec71e474ac10b4e89d938e54e416fe77cbc
pilot114 <pilot114@bk.ru>
2014-08-28 23:54:26 +0700

    up realm realm

--- public/realm.js
+++ public/realm.js
@@ -5,8 +5,21 @@ var realm = new autobahn.Connection({
     realm: 'realm'
 });
 realm.onopen = function (session) {
+    function onwelcome(args) {
+        // connect to game realm
+        var game = new autobahn.Connection({
+            url: 'ws://127.0.0.1:9090',
+            realm: gameId
+        });
+        // unsub for realm...
+    }
 
-//    session.subscribe('realm.add', onevent);
-    session.publish('realm.create', [gameId]);
+    function onfull(args) {
+        console.log('Sorry, game is full');
+    }
+
+    session.subscribe('realm.welcome', onwelcome);
+    session.subscribe('realm.full', onfull);
+    session.publish('realm.init', [gameId, session.id]);
 };
 realm.open();
--- run/Realm.php
+++ run/Realm.php
@@ -5,35 +5,44 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use Thruway\ClientSession;
 use Thruway\Connection;
 
-$onClose = function ($msg) {
-    echo $msg;
-};
-
 $connection = new Connection([
     "realm"   => 'realm',
-    "onClose" => $onClose,
+    "onClose" => function ($msg) { echo $msg; },
     "url"     => 'ws://127.0.0.1:9090',
 ]);
 
 $connection->on('open',
     function (ClientSession $session) {
+        $session->subscribe('realm.init', function ($args) use($session) {
 
-        $session->subscribe('realm.create', function ($args) {
-            // check has realm ..
-            echo "Create new realm with id: : {$args[0]}\n";
-            // create realm ...
-            // or return error...
+            static $realms = [];
+            $gameId   = $args[0];
+            $playerId = $args[1];
+            var_dump($realms);
+
+            if(array_key_exists($gameId, $realms)){
+
+                if(count($realms[$gameId]) > 2) {
+                    echo "realm is full: {$gameId}\n";
+                    $session->publish('realm.full', ['Game is full']);
+                } else {
+                    echo "Add new player in realm: {$gameId}\n";
+                    if(!in_array($playerId, $realms[$gameId])) {
+                        $realms[$gameId][] = $playerId;
+                    }
+                    $session->publish('realm.welcome', ['Welcome!']);
+                }
+
+            } else {
+                // create realm ...
+                echo "Create new realm with id: : {$gameId}\n";
+                $realms[$gameId] = [];
+                if(!in_array($playerId, $realms[$gameId])) {
+                    $realms[$gameId][] = $playerId;
+                }
+                $session->publish('realm.welcome', ['Welcome!']);
+            }
         });
-
-//        $session->publish('com.myapp.hello', ['Hello, world from PHP!!!'], [], ["acknowledge" => true])
-//            ->then(
-//                function () {
-//                    echo "Publish Acknowledged!\n";
-//                },
-//                function ($error) {
-//                    echo "Publish Error {$error}\n";
-//                }
-//            );
     }
 );
 
